6149ea83a676ab202bcc74c7fb9028d49e747ded
Simran Thiara <simranthiara@Simrans-MacBook-Air.local>
2020-09-13 23:27:01 -0700

    Updates

---
+++ precache-manifest.5ee1142df940f196a7f07fb159bda1c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee84aceaf7aeceedf2bbec3b0942c9f7",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "5e22927c89b841067081",
+    "url": "/ravenous/static/css/main.f8952ea1.chunk.css"
+  },
+  {
+    "revision": "9925d90d1471d1ce66f7",
+    "url": "/ravenous/static/js/2.99d41395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.99d41395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e22927c89b841067081",
+    "url": "/ravenous/static/js/main.a5cfe22d.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravenous/static/media/logo.5d5d9eef.svg"
+  }
+]);
